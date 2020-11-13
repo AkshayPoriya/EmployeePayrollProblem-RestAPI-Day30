1d777fc0c8ff720a02f66677d644e7d4c81ff701
AkshayPoriya <akshayporiya1234@gmail.com>
2020-11-13 06:16:02 +0530

    [Akshay] Retrieve Data From Json Server

--- EmployeePayrollMSTest/TestRestAPI.cs
+++ EmployeePayrollMSTest/TestRestAPI.cs
@@ -11,11 +11,8 @@ namespace EmployeePayrollMSTest
     [TestClass]
     public class TestRestAPI
     {
-        //declaring restclient variable
         RestClient client;
-        /// <summary>
-        /// Setups this instance for the client by giving url along with port.
-        /// </summary>
+        
         [TestInitialize]
         public void Setup()
         {
@@ -25,13 +22,10 @@ namespace EmployeePayrollMSTest
         /// Gets the employee list in the form of irestresponse. 
         /// </summary>
         /// <returns>IRestResponse response</returns>
-        private IRestResponse getEmployeeList()
+        private IRestResponse GetEmployeeDetails()
         {
-            //arrange
             //makes restrequest for getting all the data from json server by giving table name and method.get
             RestRequest request = new RestRequest("/employees", Method.GET);
-
-            //act
             //executing the request using client and saving the result in IrestResponse.
             IRestResponse response = client.Execute(request);
             return response;
@@ -40,11 +34,11 @@ namespace EmployeePayrollMSTest
         /// Ons the calling get API return employee list.
         /// </summary>
         [TestMethod]
-        public void onCallingGetApi_ReturnEmployeeList()
+        public void MakingGetRequestToReturnIRestResponseObject()
         {
-            //gets the irest response from getemployee list method
-            IRestResponse response = getEmployeeList();
-            //assert
+            //Arrange
+            IRestResponse response = GetEmployeeDetails();
+            //Assert
             Assert.AreEqual(response.StatusCode, HttpStatusCode.OK);
             //adding the data into list from irestresponse by using deserializing.
             List<Employee> dataResponse = JsonConvert.DeserializeObject<List<Employee>>(response.Content);
@@ -52,7 +46,7 @@ namespace EmployeePayrollMSTest
             foreach (Employee employee in dataResponse)
             {
                 Console.WriteLine("Id: " + employee.id + " Name: " + employee.name + " Salary: " + employee.salary);
-            }            
+            }
         }
     }
 }
